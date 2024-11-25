8879919906e60d8e7deb4fc34871bab02cc21f9e
gomszab <gombosszabolcs88@gmail.com>
2024-11-25 20:10:14 +0100

    elagazas bevezetese

--- main.js
+++ main.js
@@ -24,8 +24,7 @@ const array = [
 const menuContainer = document.createElement('div'); // Letrehozok egy div elemet, ami a tablazatomat fogja tartalmazni
 document.body.appendChild(menuContainer) // Hozzateszem bodyhoz
 
-for(let i = 0; i< array.length; i++){ // vegigiteralok az arrayen egy novekmenyes ciklus segitsegevel
-    const currentElement = array[i]; // block szinten definialok egy valtozot, amibe beleteszem az array aktualis ciklusvaltozo szamu elemet
+for(const currentElement of array){ // vegigiteralok a cikluson a ciklusvaltozo az aktualis elem
     const row = document.createElement('div'); // Letrehozok egy div elemet
     menuContainer.appendChild(row); // Hozzafuzom ezt a sort a tablazatomhoz
     
@@ -36,29 +35,9 @@ for(let i = 0; i< array.length; i++){ // vegigiteralok az arrayen egy novekmenye
     const rowFirstName =document.createElement('span'); // Letrehozok egy uj span elemet
     rowFirstName.innerHTML = currentElement.firstName; // a cella tartalma az aktualis elem firstname tulajdonsaga
     row.appendChild(rowFirstName); // Hozzafuzom a most letrehozott spant a korabban letrehozott sorhoz
-    
-    const rowFirstName2 =document.createElement('span'); // Letrehozok egy uj span elemet
-    rowFirstName2.innerHTML = currentElement.firstName2; // a cella tartalma az aktualis elem firstname2 tulajdonsaga
-    row.appendChild(rowFirstName2); // Hozzafuzom a most letrehozott spant a korabban letrehozott sorhoz
+    if(currentElement.firstName2 !== undefined){ // vizsgalom a firstName2 erteket a ciklusvaltozo objektumanak, ha az nem egyenlo undefineddel, akkor vegrehajtja az elagazason beluli utasitasokat
+        const rowFirstName2 =document.createElement('span'); // Letrehozok egy uj span elemet
+        rowFirstName2.innerHTML = currentElement.firstName2; // a cella tartalma az aktualis elem firstname2 tulajdonsaga
+        row.appendChild(rowFirstName2); // Hozzafuzom a most letrehozott spant a korabban letrehozott sorhoz
+    } 
 }
-
-/**
- * Ennel a lepesnel a feluleten megjelenhet undefined
- * alternativa lehet
-for(const currentElement of array){
-    const row = document.createElement('div'); // Letrehozok egy div elemet
-    menuContainer.appendChild(row); // Hozzafuzom ezt a sort a tablazatomhoz
-    
-    const rowLastName =document.createElement('span'); // Letrehozok egy uj span elemet
-    rowLastName.innerHTML = currentElement.lastName; // a cella tartalma az aktualis elem lastname tulajdonsaga
-    row.appendChild(rowLastName); // Hozzafuzom a most letrehozott spant a korabban letrehozott sorhoz
-    
-    const rowFirstName =document.createElement('span'); // Letrehozok egy uj span elemet
-    rowFirstName.innerHTML = currentElement.firstName; // a cella tartalma az aktualis elem firstname tulajdonsaga
-    row.appendChild(rowFirstName); // Hozzafuzom a most letrehozott spant a korabban letrehozott sorhoz
-    
-    const rowFirstName2 =document.createElement('span'); // Letrehozok egy uj span elemet
-    rowFirstName2.innerHTML = currentElement.firstName2; // a cella tartalma az aktualis elem firstname2 tulajdonsaga
-    row.appendChild(rowFirstName2); // Hozzafuzom a most letrehozott spant a korabban letrehozott sorhoz
-}
- */
